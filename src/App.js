9a728b3a5fca1a5719a94a1e988b91f23c8a245a
Craig <craigpereira1337@gmail.com>
2020-08-26 13:14:17 +0530

    Added feature: Search & retrive a character's picture and info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./css/App.css";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/Searchbar";
 import CardList from "./components/CardList";
 
 class App extends Component {
+  state = {
+    characters: [],
+    search: "",
+  };
+  setSearchField = (e) => {
+    this.setState({
+      search: e.target.value,
+    });
+  };
+  handleSumbit = (e) => {
+    e.preventDefault();
+    const path = "https://www.breakingbadapi.com/api/characters?name=";
+    const name = this.state.search;
+    axios.get(`${path + name}`).then((res) => {
+      this.setState({
+        characters: res.data,
+      });
+    });
+  };
   render() {
     return (
       <div className="App">
         <Navbar />
-        <SearchBar />
-        <CardList />
+        <SearchBar
+          characters={this.state.characters}
+          setSearchField={this.setSearchField}
+          handleSumbit={this.handleSumbit}
+        />
+        <CardList characters={this.state.characters} />
       </div>
     );
   }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "../css/Searchbar.css";
 
-const Searchbar = () => {
+const Searchbar = ({ characters, setSearchField, handleSumbit }) => {
   return (
     <div className="searchbar">
-      <form className="form">
+      <form className="form" onSubmit={handleSumbit}>
         <div className="row">
           <div className="input-field">
             <input
               type="text"
               placeholder="  Say my name"
               className="validate"
+              onChange={setSearchField}
             />
           </div>
         </div>
