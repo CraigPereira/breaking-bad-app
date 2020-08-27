133271872fdfaa8bca0f537b922556b05b743e08
Craig <craigpereira1337@gmail.com>
2020-08-27 23:06:35 +0530

    Updates

---
+++ precache-manifest.2338ffeaa81d423031a1fbcb3acfc3be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1819e435e28b5f15f26e2c55912c6a",
+    "url": "/breaking-bad-app/index.html"
+  },
+  {
+    "revision": "c01da05e4d68e25717f8",
+    "url": "/breaking-bad-app/static/css/main.318663cd.chunk.css"
+  },
+  {
+    "revision": "f137feda305833287c88",
+    "url": "/breaking-bad-app/static/js/2.7512ff8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-app/static/js/2.7512ff8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01da05e4d68e25717f8",
+    "url": "/breaking-bad-app/static/js/main.1556a626.chunk.js"
+  },
+  {
+    "revision": "e8870194d7da1c7e036e",
+    "url": "/breaking-bad-app/static/js/runtime-main.ed651a58.js"
+  },
+  {
+    "revision": "1a8b14b9ffbe094223231e302e818f10",
+    "url": "/breaking-bad-app/static/media/breaking-bad-logo.1a8b14b9.png"
+  }
+]);
